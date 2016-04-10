043a86ba072c742ebac1c6e583e801402792cda1
Du-Haitao <haitao_du@outlook.com>
2016-04-10 22:03:56 +0800

    Initial commit

---
+++ passer-rating/rating.c
@@ -0,0 +1,9 @@
+//
+//  rating.c
+//  passer-rating
+//
+//  Created by 海涛 on 16/4/2.
+//  Copyright © 2016年 Du-Haitao. All rights reserved.
+//
+
+#include "rating.h"
---
+++ passer-rating/rating.h
@@ -0,0 +1,14 @@
+//
+//  rating.h
+//  passer-rating
+//
+//  Created by 海涛 on 16/4/2.
+//  Copyright © 2016年 Du-Haitao. All rights reserved.
+//
+
+#ifndef rating_h
+#define rating_h
+
+#include <stdio.h>
+
+#endif /* rating_h */
